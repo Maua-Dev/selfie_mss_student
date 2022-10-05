a91d07dbc4d1d0bae29d7df4ed41eeb1ff341ee5
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-10-04 23:11:05 -0300

    ViewModel get_student com TU

---
+++ tests/modules/get_student/test_get_student_view_model.py
@@ -0,0 +1,20 @@
+import pytest
+from src.modules.get_student.get_student_view_model import GetStudentViewModel
+from src.domain.entities.student import Student
+from src.infra.repositories.student_repository_mock import StudentRepositoryMock
+
+class Test_GetStudentViewModel():
+    def test_get_student_view_model(self):
+        repo = StudentRepositoryMock()
+        student = repo.students[0]
+        result = {
+            "ra":"21010757",
+            "name":"Victor",
+            "email":"eusousoller@gmail.com"
+        }
+        
+        studentViewModel = GetStudentViewModel(student).to_dict()
+        
+        assert studentViewModel == result
+        
+        
