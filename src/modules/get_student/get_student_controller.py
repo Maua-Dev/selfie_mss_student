36788701a8d60ab4d708c10af557973eaeca7dbc
Vitor Soller <vgsoller@gmail.com>
2022-10-06 23:26:40 -0300

    Classe sem harança n coloca ()

--- src/modules/get_student/get_student_controller.py
+++ src/modules/get_student/get_student_controller.py
@@ -4,8 +4,9 @@ from src.helpers.http.http_models import OK, BadRequest, HttpRequest, HttpRespon
 from src.modules.get_student.get_student_usecase import GetStudentUsecase
 from src.modules.get_student.get_student_view_model import GetStudentViewModel
 
-class GetStudentController():
-    def __init__(self, usecase:GetStudentUsecase):
+
+class GetStudentController:
+    def __init__(self, usecase: GetStudentUsecase):
         self.getStudentUsecase = usecase
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
@@ -14,22 +15,19 @@ class GetStudentController():
                 raise MissingParameters('ra')
             if request.query_params.get('email') is None:
                 raise MissingParameters('email')
-            
+
             student = self.getStudentUsecase(
                 ra=request.query_params["ra"],
                 email=request.query_params["email"]
             )
             viewmodel = GetStudentViewModel(student)
             return OK(viewmodel.to_dict())
-        
+
         except NoItemsFound as err:
             return NotFound(body=err.message)
-        
+
         except MissingParameters as err:
             return BadRequest(body=err.message)
-        
+
         except Exception as err:
             return InternalServerError(body=err.message)
-            
-
-
