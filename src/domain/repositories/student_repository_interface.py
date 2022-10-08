5f86c478448364733a2bcb93f2e877f46332a65f
Vitor Soller <vgsoller@gmail.com>
2022-10-08 17:14:59 -0300

    Criando pastas e atualizando a interface com o update

--- src/domain/repositories/student_repository_interface.py
+++ src/domain/repositories/student_repository_interface.py
@@ -4,8 +4,9 @@ from src.domain.entities.student import Student
 
 
 class IStudentRepository(ABC):
-    
-    def get_student(self, ra:str, email:str) -> Student:
+
+    def get_student(self, ra: str, email: str) -> Student:
         pass
 
-    
+    def update_student(self, ra: str = None, name: str = None, email: str = None):
+        pass
