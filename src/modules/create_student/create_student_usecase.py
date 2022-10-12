5e030eb97b321b9cc86650737abd1b2a7b445863
Vitor Soller <vgsoller@gmail.com>
2022-10-11 22:35:02 -0300

    Resolvendo os erros de função

--- src/modules/create_student/create_student_usecase.py
+++ src/modules/create_student/create_student_usecase.py
@@ -9,8 +9,5 @@ class CreateStudentUsecase:
         
     def __call__(self, student: Student) -> Student:
 
-        if self.repo.get_students_by_ra_or_email(ra = student.ra, email = student.email):
-            raise DuplicatedItem("ra or email")
-
         return self.repo.create_student(student)
          
