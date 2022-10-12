54f67c725301edf21ea788850498c08377cfb0b3
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-10-12 11:13:23 -0300

    usecase sem TU

--- src/modules/get_selfies_by_ra/get_selfies_by_ra_usecase.py
+++ src/modules/get_selfies_by_ra/get_selfies_by_ra_usecase.py
@@ -1,21 +1,26 @@
+from typing import List
 from src.helpers.errors.domain_errors import EntityError
 from src.helpers.errors.usecase_errors import NoItemsFound
 from src.domain.entities.student import Student
+from src.domain.entities.selfie import Selfie
 from src.domain.repositories.student_repository_interface import IStudentRepository
-class GetStudentUsecase:
+class GetSelfiesByRaUsecase:
     def __init__(self, repo:IStudentRepository):
         self.repo = repo
         
-    def __call__(self, ra: str) -> Student:
+    def __call__(self, ra: str) -> List[Selfie]:
 
         if not Student.validate_ra(ra):
             raise EntityError('ra')
 
-        student = self.repo.get_student(ra=ra)
+        if self.repo.get_student(ra=ra) ==  None:
+            raise NoItemsFound("ra")
+
+        selfies = self.repo.get_selfies_by_ra(ra=ra)
         
-        if student == None:
-            raise NoItemsFound("Student")
+        if len(selfies) == 0:
+            raise NoItemsFound("Selfie")
         
-        return student
+        return selfies
         
         
