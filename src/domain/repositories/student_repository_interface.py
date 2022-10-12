4011668567ee9cf9d58b23b2c7c4eba54f6b5052
Vitor Soller <vgsoller@gmail.com>
2022-10-12 20:01:37 -0300

    Retirando a interface do coverage #Ronaldinho10

--- src/domain/repositories/student_repository_interface.py
+++ src/domain/repositories/student_repository_interface.py
@@ -1,24 +1,25 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import List, Tuple
 from src.domain.entities.student import Student
 from src.domain.entities.selfie import Selfie
 
 class IStudentRepository(ABC):
 
+    @abstractmethod
     def get_student(self, ra: str) -> Student:
         pass
-
+    @abstractmethod    
     def update_student(self, ra: str, new_name: str = None, new_email: str = None) -> Student:
         pass
-
+    @abstractmethod    
     def delete_student(self, ra: str) -> Student:
         pass
-    
+    @abstractmethod    
     def create_student(self, student: Student) -> Student:
         pass
-
+    @abstractmethod    
     def get_selfies_by_ra(self, ra: str) -> Tuple[List[Selfie], Student]:
         pass
-    
+    @abstractmethod    
     def get_selfie(self, ra: str, idSelfie: int) -> Selfie:
         pass
