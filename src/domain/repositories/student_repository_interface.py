d2bc5ef87b0591c0c0231cd8359b07dff3c1a65e
Vitor Soller <vgsoller@gmail.com>
2022-10-12 20:07:17 -0300

    Arrumando espaçamento

--- src/domain/repositories/student_repository_interface.py
+++ src/domain/repositories/student_repository_interface.py
@@ -8,18 +8,24 @@ class IStudentRepository(ABC):
     @abstractmethod
     def get_student(self, ra: str) -> Student:
         pass
+    
     @abstractmethod    
     def update_student(self, ra: str, new_name: str = None, new_email: str = None) -> Student:
         pass
+
     @abstractmethod    
     def delete_student(self, ra: str) -> Student:
         pass
+    
     @abstractmethod    
     def create_student(self, student: Student) -> Student:
         pass
+
     @abstractmethod    
     def get_selfies_by_ra(self, ra: str) -> Tuple[List[Selfie], Student]:
         pass
+
     @abstractmethod    
     def get_selfie(self, ra: str, idSelfie: int) -> Selfie:
         pass
+    
