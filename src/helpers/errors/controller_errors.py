6007103800b577f3b9534c36cf5d7e0142384e77
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-10-12 19:07:06 -0300

    adiciona WrongTypeParameter

--- src/helpers/errors/controller_errors.py
+++ src/helpers/errors/controller_errors.py
@@ -4,3 +4,6 @@ from src.helpers.errors.base_error import BaseError
 class MissingParameters(BaseError):
     def __init__(self, message: str):
         super().__init__(f'Field {message} is missing')
+class WrongTypeParameter(BaseError):
+    def __init__(self, fieldName: str, fieldTypeExpected: str, fieldTypeReceived: str):
+        super().__init__(f'Field {fieldName} isn\'t in the right type.\nReceived: {fieldTypeExpected}\nExpected: {fieldTypeReceived}')
