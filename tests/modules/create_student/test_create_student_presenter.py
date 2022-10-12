77544768c35905a31275a8dee7205eb3f578cc01
Vitor Soller <vgsoller@gmail.com>
2022-10-11 23:18:51 -0300

    duplicated ra em create_student_presenter

--- tests/modules/create_student/test_create_student_presenter.py
+++ tests/modules/create_student/test_create_student_presenter.py
@@ -255,3 +255,65 @@ class Test_createStudentPresenter:
         response = lambda_handler(event, None)
         assert response["statusCode"] == 400
         assert response["body"] == expected
+
+        def test_create_student_duplicated_ra(self):
+            event = {
+                "version": "2.0",
+                "routeKey": "$default",
+                "rawPath": "/my/path",
+                "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+                "cookies": [
+                    "cookie1",
+                    "cookie2"
+                ],
+                "headers": {
+                    "header1": "value1",
+                    "header2": "value1,value2"
+                },
+                "queryStringParameters": {
+                    "ra": "21014442",
+                    "name": "Scott Flansburg",
+                    "email": "eusouoawsboy@amazon.com",
+                    "parameter2": "value"
+                },
+                "requestContext": {
+                    "accountId": "123456789012",
+                    "apiId": "<urlid>",
+                    "authentication": None,
+                    "authorizer": {
+                        "iam": {
+                            "accessKey": "AKIA...",
+                            "accountId": "111122223333",
+                            "callerId": "AIDA...",
+                            "cognitoIdentity": None,
+                            "principalOrgId": None,
+                            "userArn": "arn:aws:iam::111122223333:user/example-user",
+                            "userId": "AIDA..."
+                        }
+                    },
+                    "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                    "domainPrefix": "<url-id>",
+                    "http": {
+                        "method": "POST",
+                        "path": "/my/path",
+                        "protocol": "HTTP/1.1",
+                        "sourceIp": "123.123.123.123",
+                        "userAgent": "agent"
+                    },
+                    "requestId": "id",
+                    "routeKey": "$default",
+                    "stage": "$default",
+                    "time": "12/Mar/2020:19:03:58 +0000",
+                    "timeEpoch": 1583348638390
+                },
+                "body": "Hello from client!",
+                "pathParameters": None,
+                "isBase64Encoded": None,
+                "stageVariables": None
+            }
+
+            expected = 'The item alredy exists for this ra'
+
+            response = lambda_handler(event, None)
+            assert response["statusCode"] == 400
+            assert response["body"] == expected
