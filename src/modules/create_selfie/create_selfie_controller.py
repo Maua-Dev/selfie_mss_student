c816634fbf6887ec05bd20d4644b17ee9d429840
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-10-13 20:18:02 -0300

    create_selfie controller

---
+++ src/modules/create_selfie/create_selfie_controller.py
@@ -0,0 +1,40 @@
+from src.domain.entities.student import Student
+from src.helpers.errors.domain_errors import EntityError
+from src.helpers.errors.usecase_errors import DuplicatedItem, NoItemsFound
+from src.helpers.errors.controller_errors import MissingParameters
+from src.helpers.http.http_models import BadRequest, Created, HttpRequest, HttpResponse, InternalServerError, NotFound
+from src.modules.create_selfie.create_selfie_usecase import CreateSelfieUsecase
+from src.modules.create_selfie.create_selfie_viewmodel import CreateSelfieViewModel
+
+
+class CreateSelfieController:
+    def __init__(self, usecase: CreateSelfieUsecase):
+        self.createSelfieUsecase = usecase
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        try:
+            if request.query_params.get('ra') is None:
+                raise MissingParameters('ra')
+
+            if request.query_params.get('url') is None:
+                raise MissingParameters('url')
+
+            student = self.createSelfieUsecase(ra=request.query_params.get('ra'), url=request.query_params.get('url'))
+            viewmodel = CreateSelfieViewModel(student)
+
+            return Created(viewmodel.to_dict())
+
+        except NoItemsFound as err:
+            return NotFound(body=err.message)
+
+        except DuplicatedItem as err:
+            return BadRequest(body=err.message)
+
+        except MissingParameters as err:
+            return BadRequest(body=err.message)
+
+        except EntityError as err:
+            return BadRequest(body=err.message)
+
+        except Exception as err:
+            return InternalServerError(body=err.args[0])
