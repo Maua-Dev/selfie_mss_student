4c888ba96e00e51c9c341496dfdb0f8427133a24
Vitor Soller <vgsoller@gmail.com>
2022-10-12 21:50:57 -0300

    delete_selfie ViewModel

---
+++ src/modules/delete_selfie/delete_selfie_viewmodel.py
@@ -0,0 +1,57 @@
+import datetime
+from typing import List
+from src.domain.enums.state_enum import STATE
+from src.domain.entities.selfie import Selfie
+from src.domain.entities.student import Student
+
+class StudentViewModel:
+    ra: str
+    name: str
+    email: str
+    
+    def __init__(self, student: Student):
+        self.ra = student.ra
+        self.name = student.name
+        self.email = student.email
+
+    def to_dict(self) -> dict:
+        return  {
+            "ra":self.ra,
+            "name":self.name,
+            "email":self.email
+        }
+
+class SelfieViewModel:
+    idSelfie: int
+    dateUpload: str
+    url: str
+    state: STATE
+
+    def __init__(self, selfie: Selfie):
+        self.idSelfie = selfie.idSelfie
+        self.dateUpload = selfie.dateUpload
+        self.url = selfie.url
+        self.state = selfie.state
+
+    def to_dict(self) -> dict:
+        return {
+            "idSelfie" : self.idSelfie,
+            "dateUpload" : self.dateUpload.isoformat(),
+            "url" : self.url,
+            "state" : self.state.value
+        }
+
+class DeleteSelfieViewModel:
+    selfie: SelfieViewModel 
+    student: StudentViewModel
+
+    def __init__(self, data: Selfie, student: Student):
+        self.selfie = SelfieViewModel(data)
+        self.student = StudentViewModel(student)
+
+    def to_dict(self) -> dict:
+        return {
+            "student": self.student.to_dict(),
+            "selfie": self.selfie.to_dict(),
+            "message": "the selfie was deleted"
+        }
---
+++ tests/modules/delete_selfie/test_delete_selfie_viewmodel.py
@@ -0,0 +1,33 @@
+import pytest
+from src.domain.entities.student import Student
+from src.infra.repositories.student_repository_mock import StudentRepositoryMock
+from src.modules.delete_selfie.delete_selfie_viewmodel import DeleteSelfieViewModel
+
+class Test_GetStudentViewModel:
+    def test_get_selfie_view_model(self):
+        repo = StudentRepositoryMock()
+        selfie = repo.selfies[0]
+        student = repo.students[0]
+        result = {
+            'student':{
+            "ra":"21010757",
+   "name":"Victor",
+            "email":"eusousoller@gmail.com"
+        },
+            'selfie':{
+ 'dateUpload': '2022-10-12T16:01:59.149927',
+            'idSelfie': 0,
+            'state': 'DECLINED',
+            'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+            },
+            'message':"the selfie was deleted"
+          }
+     
+
+
+        
+        studentViewModel = DeleteSelfieViewModel(data=selfie, student=student).to_dict()
+        
+        assert studentViewModel == result
+        
+        
