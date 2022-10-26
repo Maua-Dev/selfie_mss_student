34ea8d0bccafe14fad52491351acf5f8538fff80
Vitor Soller <vgsoller@gmail.com>
2022-10-26 11:11:26 -0300

    update_selfie check approved and rejected e refat de UTs"

--- tests/modules/update_selfie/app/test_update_selfie_presenter.py
+++ tests/modules/update_selfie/app/test_update_selfie_presenter.py
@@ -49,7 +49,7 @@ class Test_GetSelfiePresenter:
                 "time": "12/Mar/2020:19:03:58 +0000",
                 "timeEpoch": 1583348638390
             },
-             "body": '{"ra": "21010757","idSelfie": "0","new_state": "DECLINED","new_rejectionReason": "OTHER_REASON", "new_rejectionDescription":"skip bap du bap", "parameter2": "value" }',
+             "body": '{"ra": "21014440","idSelfie": "0","new_state": "DECLINED","new_rejectionReason": "OTHER_REASON", "new_rejectionDescription":"skip bap du bap", "parameter2": "value" }',
              "pathParameters": None,
             "isBase64Encoded": None,
             "stageVariables": None
@@ -57,7 +57,7 @@ class Test_GetSelfiePresenter:
 
         response = lambda_handler(event, None)
         assert response["statusCode"] == 200
-        assert json.loads(response["body"])["student"]["ra"] == "21010757"
+        assert json.loads(response["body"])["student"]["ra"] == "21014440"
         assert json.loads(response["body"])["state"] == "DECLINED"
 
     def test_update_selfie_ra_non_existent(self):
@@ -222,5 +222,117 @@ class Test_GetSelfiePresenter:
         response = lambda_handler(event, None)
         assert response["statusCode"] == 404
         assert json.loads(response["body"]) == "No items found for ra or idSelfie"
+
+  
+
+    def test_update_selfie_already_approved(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "iam": {
+                        "accessKey": "AKIA...",
+                        "accountId": "111122223333",
+                        "callerId": "AIDA...",
+                        "cognitoIdentity": None,
+                        "principalOrgId": None,
+                        "userArn": "arn:aws:iam::111122223333:user/example-user",
+                        "userId": "AIDA..."
+                    }
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "http": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": '{"ra": "21010757","idSelfie": "0","parameter2": "value" }',          "pathParameters": None,
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
+
+        response = lambda_handler(event, None)
+        assert response["statusCode"] == 403
+        assert json.loads(response["body"]) == "That action is forbidden for this Selfie" 
+
+  
+
+    def test_update_selfie_already_rejected(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "iam": {
+                        "accessKey": "AKIA...",
+                        "accountId": "111122223333",
+                        "callerId": "AIDA...",
+                        "cognitoIdentity": None,
+                        "principalOrgId": None,
+                        "userArn": "arn:aws:iam::111122223333:user/example-user",
+                        "userId": "AIDA..."
+                    }
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "http": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": '{"ra": "21002088","idSelfie": "0","parameter2": "value" }',          "pathParameters": None,
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
+
+        response = lambda_handler(event, None)
+        assert response["statusCode"] == 403
+        assert json.loads(response["body"]) == "That action is forbidden for this Selfie" 
 
   
