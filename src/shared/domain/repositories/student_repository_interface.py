4fe1ced23397df23dcbe030ed4799ffc2badce6b
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-10-25 21:39:54 -0300

    cria check_student_has_approved_selfie

--- src/shared/domain/repositories/student_repository_interface.py
+++ src/shared/domain/repositories/student_repository_interface.py
@@ -51,4 +51,7 @@ class IStudentRepository(ABC):
     def update_selfie(self, ra: str, idSelfie: int, new_state: STATE = None, new_rejectionReason: REJECTION_REASON = None, new_rejectionDescription: str = None) -> Selfie:
         pass
     
+    @abstractmethod    
+    def check_student_has_approved_selfie(self, ra: str) -> bool:
+        pass
     
