e72411caca6b67a88246c2fd5a626542dd1a07aa
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-10-26 23:01:59 -0300

    get_all_students_presenter

--- src/modules/get_all_students/app/get_all_students_presenter.py
+++ src/modules/get_all_students/app/get_all_students_presenter.py
@@ -1,1 +1,19 @@
 
+from src.modules.get_all_students.app.get_all_students_controller import GetAllStudentsController
+from src.modules.get_all_students.app.get_all_students_usecase import GetAllStudentsUsecase
+from src.shared.infra.repositories.student_repository_mock import StudentRepositoryMock
+from src.shared.helpers.http.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+
+
+def lambda_handler(event, context):
+    repo = StudentRepositoryMock() 
+    usecase = GetAllStudentsUsecase(repo)
+    controller = GetAllStudentsController(usecase)
+
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+
+    return httpResponse.toDict()
+
