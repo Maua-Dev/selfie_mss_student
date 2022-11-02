b9a032f84b89cad4e84c85fc729115daca0dc943
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-11-02 19:07:53 -0300

    adiciona automaticReview no VM do get_selfie

--- src/modules/get_selfie/app/get_selfie_viewmodel.py
+++ src/modules/get_selfie/app/get_selfie_viewmodel.py
@@ -1,7 +1,48 @@
 from src.shared.domain.enums.rejection_reason_enum import REJECTION_REASON
 from src.shared.domain.enums.state_enum import STATE
 from src.shared.domain.entities.selfie import Selfie
+from src.shared.domain.entities.automatic_review import AutomaticReview
+from src.shared.domain.entities.label import Label
 
+class LabelViewModel:
+    name: str
+    coords: dict[str, float]
+    confidence: float
+    parents: list[str] 
+    
+    def __init__(self, label:Label):
+        self.name = label.name            
+        self.coords = label.coords            
+        self.confidence = label.confidence        
+        self.parents = label.parents        
+
+        
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "coords": self.coords,
+            "confidence": self.confidence,
+            "parents": self.parents 
+        }
+
+class AutomaticReviewViewModel:
+    automaticallyRejected: bool
+    rejectionReason: REJECTION_REASON
+    labels: list[LabelViewModel]
+    
+    def __init__(self, automaticReview:AutomaticReview):
+            self.automaticallyRejected = automaticReview.automaticallyRejected
+            self.rejectionReason = automaticReview.rejectionReason
+            self.labels = [LabelViewModel(label) for label in automaticReview.labels]
+
+    
+    def to_dict(self):
+        return {
+            "automaticallyRejected": self.automaticallyRejected,    
+            "rejectionReason": self.rejectionReason.value,
+            "labels": [label.to_dict() for label in self.labels]
+        }
+        
 class GetSelfieViewModel:
     idSelfie: int
     dateCreated: str
@@ -9,6 +50,7 @@ class GetSelfieViewModel:
     state: STATE
     rejectionReason: REJECTION_REASON
     rejectionDescription: str
+    automaticReview: AutomaticReviewViewModel
 
     def __init__(self, selfie: Selfie):
         self.idSelfie = selfie.idSelfie
@@ -17,6 +59,7 @@ class GetSelfieViewModel:
         self.state = selfie.state
         self.rejectionReason =  selfie.rejectionReason
         self.rejectionDescription = selfie.rejectionDescription
+        self.automaticReview = AutomaticReviewViewModel(selfie.automaticReview)
 
     def to_dict(self) -> dict:
         return {
@@ -26,5 +69,6 @@ class GetSelfieViewModel:
             "state" : self.state.value,
             "rejectionReason": self.rejectionReason.value,
             "rejectionDescription": self.rejectionDescription,
+            "automaticReview": self.automaticReview.to_dict(),
             "message": "the selfie was retriven"
         }
