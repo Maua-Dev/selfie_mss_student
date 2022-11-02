f0a6828e96afcaf4d7d6bc82cd4d0859882b4ac3
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-11-02 19:37:23 -0300

    adicion automaticReview do get_all_studentsVM (🥵)

--- src/modules/get_all_students/app/get_all_students_viewmodel.py
+++ src/modules/get_all_students/app/get_all_students_viewmodel.py
@@ -4,7 +4,48 @@ from src.shared.domain.entities.selfie import Selfie
 from src.shared.domain.enums.student_state_enum import STUDENT_STATE
 from src.shared.domain.enums.state_enum import STATE
 from src.shared.domain.enums.rejection_reason_enum import REJECTION_REASON
+from src.shared.domain.entities.automatic_review import AutomaticReview
+from src.shared.domain.entities.label import Label
 
+class LabelViewModel:
+    name: str
+    coords: dict[str, float]
+    confidence: float
+    parents: list[str] 
+    
+    def __init__(self, label:Label):
+        self.name = label.name            
+        self.coords = label.coords            
+        self.confidence = label.confidence        
+        self.parents = label.parents        
+
+        
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "coords": self.coords,
+            "confidence": self.confidence,
+            "parents": self.parents 
+        }
+
+class AutomaticReviewViewModel:
+    automaticallyRejected: bool
+    rejectionReason: REJECTION_REASON
+    labels: list[LabelViewModel]
+    
+    def __init__(self, automaticReview:AutomaticReview):
+            self.automaticallyRejected = automaticReview.automaticallyRejected
+            self.rejectionReason = automaticReview.rejectionReason
+            self.labels = [LabelViewModel(label) for label in automaticReview.labels]
+
+    
+    def to_dict(self):
+        return {
+            "automaticallyRejected": self.automaticallyRejected,    
+            "rejectionReason": self.rejectionReason.value,
+            "labels": [label.to_dict() for label in self.labels]
+        }
+        
 class SelfieViewModel:
     idSelfie: int
     dateCreated: str
@@ -12,6 +53,7 @@ class SelfieViewModel:
     state: STATE
     rejectionReason: REJECTION_REASON
     rejectionDescription: str
+    automaticReview: AutomaticReviewViewModel
 
     def __init__(self, selfie: Selfie):
         self.idSelfie = selfie.idSelfie
@@ -20,6 +62,7 @@ class SelfieViewModel:
         self.state = selfie.state
         self.rejectionReason =  selfie.rejectionReason
         self.rejectionDescription = selfie.rejectionDescription
+        self.automaticReview = AutomaticReviewViewModel(selfie.automaticReview)
 
     def to_dict(self) -> dict:
         return {
@@ -28,6 +71,7 @@ class SelfieViewModel:
             "url" : self.url,
             "state" : self.state.value,
             "rejectionReason": self.rejectionReason.value,
+            "automaticReview": self.automaticReview.to_dict(),
             "rejectionDescription": self.rejectionDescription,
         }
 
