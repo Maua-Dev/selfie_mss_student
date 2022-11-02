da2360bfdb202863f2b896db1f4203fd358ffa43
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-11-02 11:06:17 -0300

    cria função read_automatic_review (dict p/ entity)

---
+++ src/shared/helpers/functions/read_automatic_review.py
@@ -0,0 +1,33 @@
+from src.shared.domain.entities.automatic_review import AutomaticReview
+from src.shared.domain.enums.rejection_reason_enum import REJECTION_REASON
+from src.shared.domain.entities.label import Label
+from src.shared.helpers.errors.domain_errors import EntityError
+
+def read_automatic_review(automaticReview: dict) -> AutomaticReview: 
+    try:
+        labels = list()
+
+        for label_dict in automaticReview["labels"]:
+            label = Label(
+                name = label_dict['name'],
+                coords = {
+                            "Width": float(label_dict['coords']['Width']),
+                            "Height": float(label_dict['coords']['Height']),
+                            "Left": float(label_dict['coords']["Left"]),
+                            "Top": float(label_dict['coords']["Top"])
+                        },
+                confidence= float(label_dict["confidence"]),
+                parents= label_dict["parents"]
+            )
+            labels.append(label)
+
+        result = AutomaticReview(
+            automaticallyRejected=automaticReview['automaticallyRejected'] == "True",
+            rejectionReason=REJECTION_REASON[automaticReview["rejectionReason"]],
+            labels=labels,
+        )
+        
+        return result
+
+    except:
+        raise EntityError("automaticReview")
---
+++ tests/shared/helpers/test_read_automatic_review.py
@@ -0,0 +1,79 @@
+import pytest
+from src.shared.domain.enums.rejection_reason_enum import REJECTION_REASON
+from src.shared.helpers.functions.read_automatic_review import read_automatic_review
+from src.shared.domain.entities.automatic_review import AutomaticReview
+from src.shared.domain.entities.label import Label
+from src.shared.domain.enums.state_enum import STATE
+
+class Test_read_automatic_review:
+    def test_read_automatic_review(self):
+        automatic_review_dict = {
+            "automaticallyRejected": "True",
+            "rejectionReason": "COVERED_FACE",
+            "labels": [{
+                            "name": "Glasses",
+                            "coords": {
+                                    "Width": "0.6591288447380066",
+                                    "Height": "0.17444363236427307",
+                                    "Left": "0.19148917496204376",
+                                    "Top": "0.3813813030719757"
+                            },
+                            "confidence": "94.5357666015625",
+                            "parents": ["Accessories"],
+                        },
+                        {
+                            "name": "Blalblas",
+                            "coords": {
+                                    "Width": "0.6591288480066",
+                                    "Height": "0.1744236427307",
+                                    "Left": "0.19148916204376",
+                                    "Top": "0.3813813719757"
+                            },
+                            "confidence": "95.5366015625",
+                            "parents": ["ASODnoasdsa", "nmdokasnkndkasnkd"],
+                        }]
+                  }
+        
+        result = read_automatic_review(automaticReview=automatic_review_dict)
+        
+        expected = AutomaticReview(
+            automaticallyRejected = True,
+            rejectionReason = REJECTION_REASON.COVERED_FACE,
+            labels = [
+                Label(
+                    name="Glasses",
+                    coords={
+                                "Width": 0.6591288447380066,
+                                "Height": 0.17444363236427307,
+                                "Left": 0.19148917496204376,
+                                "Top": 0.3813813030719757
+                            },
+                    confidence=94.5357666015625,
+                    parents=["Accessories"],
+                ),
+                Label(
+                    name="Blalblas",
+                    coords={
+                                "Width": 0.6591288480066,
+                                "Height": 0.1744236427307,
+                                "Left": 0.19148916204376,
+                                "Top": 0.3813813719757
+                            },
+                    confidence=95.5366015625,
+                    parents=["ASODnoasdsa", "nmdokasnkndkasnkd"],
+                )
+            ]
+        )
+        
+        assert result.automaticallyRejected  == expected.automaticallyRejected
+        assert result.rejectionReason  == expected.rejectionReason
+        
+        assert result.labels[0].name  == expected.labels[0].name
+        assert result.labels[0].coords  == expected.labels[0].coords
+        assert result.labels[0].confidence  == expected.labels[0].confidence
+        assert result.labels[0].parents  == expected.labels[0].parents
+        
+        assert result.labels[1].name  == expected.labels[1].name
+        assert result.labels[1].coords  == expected.labels[1].coords
+        assert result.labels[1].confidence  == expected.labels[1].confidence
+        assert result.labels[1].parents  == expected.labels[1].parents
