3ec900ebfc386d30e8f13da2282851d9766c821f
Bruno Vilardi <brunovilardibueno@gmail.com>
2022-11-04 17:55:15 -0300

    fixing pathing

--- iac/adjust_layer_directory.py
+++ iac/adjust_layer_directory.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 import sys
 
 IAC_DIRECTORY_NAME = "iac"
@@ -10,7 +11,9 @@ LAMBDA_LAYER_PREFIX = os.path.join("python", "src")
 def adjust_layer_directory(shared_dir_name: str, destination: str):
     # Get the root directory of the source directory
     print(f"os.getcwd().split(os.sep): {os.getcwd().split(os.sep)}")
-    os_path_index_to_root = os.getcwd().split(os.sep).index("selfie_mss_student")
+    print(f"os.path.abspath(os.path.join(os.getcwd(), os.pardir)).split(os.sep): {os.path.abspath(os.path.join(os.getcwd(), os.pardir)).split(os.sep)}")
+    # os_path_index_to_root = os.getcwd().split(os.sep).index("selfie_mss_student")
+    os_path_index_to_root = os.path.abspath(os.path.join(os.getcwd(), os.pardir)).split(os.sep).index("selfie_mss_student")
     root_directory = os.sep.join(os.getcwd().split(os.sep)[:os_path_index_to_root + 1])
 
     # Get the destination and source directory
@@ -25,3 +28,7 @@ def adjust_layer_directory(shared_dir_name: str, destination: str):
     shutil.copytree(source_directory, os.path.join(destination_directory, LAMBDA_LAYER_PREFIX, shared_dir_name))
     print(
         f"Copying files from {source_directory} to {os.path.join(destination_directory, LAMBDA_LAYER_PREFIX, shared_dir_name)}")
+
+
+if __name__ == '__main__':
+    adjust_layer_directory(shared_dir_name="shared", destination="lambda_layer_out_temp")
