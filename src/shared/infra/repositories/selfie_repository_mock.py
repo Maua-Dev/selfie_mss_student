a79773651a17ee021baf30d3c1b88bf3f6c8e57c
Vitor Soller <vgsoller@gmail.com>
2022-11-08 16:01:14 -0300

    adiciona selfie_repo_interface e mock com fução request_upload_selfie

---
+++ src/shared/infra/repositories/selfie_repository_mock.py
@@ -0,0 +1,27 @@
+import datetime
+import uuid
+from src.shared.domain.repositories.selfie_repository_interface import ISelfieRepository
+
+
+class StudentRepositoryMock(ISelfieRepository):
+
+        
+    def uuid_pic_generator_generate(self, ra: str):
+
+        object_name = f"{ra}/selfie-{datetime.today().strftime('%Y-%m-%d-%H:%M:%S')}-{str(uuid.uuid4())[:5]}.jpeg"
+        return object_name
+
+
+    def request_upload_selfie(self, ra: str, name: str, email: str, url: str) -> dict:
+        return {
+                "url":"https://test-selfie-bucket.s3.amazonaws.com/",
+                "fields":{
+                    "x-amz-meta-ra":f"{ra}",
+                    "x-amz-meta-name":f"{name}",
+                    "x-amz-meta-email":f"{email}",
+                    "key":self.uuid_pic_generator_generate(ra),
+                    "AWSAccessKeyId":f"ACCESSKEY-{ra}",
+                    "policy":f"POLICY-{ra}",
+                    "signature":f"SIGNATURE-{ra}"
+                }
+              }
