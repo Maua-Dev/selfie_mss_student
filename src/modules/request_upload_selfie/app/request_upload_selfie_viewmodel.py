9177fff9e0b1658b85f88f9523b85db41afb1d0e
Vitor Soller <vgsoller@gmail.com>
2022-11-10 21:26:03 -0300

    request_upload_selfie_viewmodel muda ordem dos fields no

--- src/modules/request_upload_selfie/app/request_upload_selfie_viewmodel.py
+++ src/modules/request_upload_selfie/app/request_upload_selfie_viewmodel.py
@@ -6,4 +6,15 @@ class RequestUploadSelfieViewModel:
       self.presignedPost = presignedPost
 
     def to_dict(self) -> dict:
-        return self.presignedPost
+        return {
+          "url":self.presignedPost["url"],
+          "fields":{
+              "key":self.presignedPost["fields"]["key"],
+              "x-amz-meta-ra": self.presignedPost["fields"]["x-amz-meta-ra"],
+              "x-amz-meta-name":self.presignedPost["fields"]["x-amz-meta-name"],
+              "x-amz-meta-email": self.presignedPost["fields"]["x-amz-meta-email"],
+              "AWSAccessKeyId":self.presignedPost["fields"]["AWSAccessKeyId"],
+              "policy":self.presignedPost["fields"]["policy"],
+              "signature":self.presignedPost["fields"]["signature"]
+          }
+        }
