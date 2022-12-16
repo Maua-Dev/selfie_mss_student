0bede3348098a86338afdc898695d6bbe5d3f6bf
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-12-16 15:58:02 -0300

    término do DTO

--- src/shared/infra/dtos/reviewer_dynamo_dto.py
+++ src/shared/infra/dtos/reviewer_dynamo_dto.py
@@ -29,6 +29,23 @@ class ReviewerDynamoDTO:
             "email": self.email,
             "active": self.active
         }
-        
+    
+    @staticmethod
+    def from_dynamo(reviewer_data: dict) -> "ReviewerDynamoDTO":
+        return ReviewerDynamoDTO(
+            ra=reviewer_data["ra"],
+            name=reviewer_data["name"],
+            email=reviewer_data["email"],
+            active=reviewer_data["active"]
+        )
+    
+    def to_entity(self) -> Reviewer:
+        return Reviewer(
+            ra=self.ra,
+            name=self.name,
+            email=self.email,
+            active=self.active,
+        )
+    
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
--- tests/shared/infra/dtos/test_reviewer_dynamo_dto.py
+++ tests/shared/infra/dtos/test_reviewer_dynamo_dto.py
@@ -26,4 +26,16 @@ class Test_ReviewerDynamoDTO:
                                                 "active":reviewer_dto.active
                                             }
         
-    
+    def test_from_dynamo(self):
+        repo = StudentRepositoryMock()     
+        
+        reviewer_dynamo = {'Item': {'name': 'Mauro Crapino', 'SK': '03026', 'active': True, 'PK': 'reviewer#03026', 'entity': 'reviewer', 'email': 'mauro@maua.br', 'ra': '03026'}, 'ResponseMetadata': {'RequestId': 'dc2ed159-1780-4994-8ba1-57b4f5f307d4', 'HTTPStatusCode': 200, 'HTTPHeaders': {'date': 'Fri, 16 Dec 2022 18:43:58 GMT', 'content-type': 'application/x-amz-json-1.0', 'x-amz-crc32': '1749714046', 'x-amzn-requestid': 'dc2ed159-1780-4994-8ba1-57b4f5f307d4', 'content-length': '184', 'server': 'Jetty(9.4.48.v20220622)'}, 'RetryAttempts': 0}}
+        reviewer_entity = repo.reviewers[0]
+        assert ReviewerDynamoDTO.from_entity(reviewer=reviewer_entity)  == ReviewerDynamoDTO.from_dynamo(reviewer_data=reviewer_dynamo["Item"])
+        
+    def test_to_entity(self):
+        repo = StudentRepositoryMock()     
+        reviewer_entity = repo.reviewers[0]
+        reviewer_from_entity_to_entity = ReviewerDynamoDTO.from_entity(reviewer=reviewer_entity)
+        assert reviewer_entity.__dict__ == reviewer_from_entity_to_entity.__dict__
+        
